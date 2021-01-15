7623c3f67c7fe69eeae97574ff25804e14a54920
Tareq Aljamal <tareqjj@gmail.com>
2021-01-16 00:45:36 +0200

    edited Dojo Overflow a Quora clone using Sping full MVC

--- Spring Data Two/Dojo Overflow/src/main/java/com/springdatatwo/dojooverflow/controllers/dojoOverflowController.java
+++ Spring Data Two/Dojo Overflow/src/main/java/com/springdatatwo/dojooverflow/controllers/dojoOverflowController.java
@@ -7,15 +7,12 @@ import com.springdatatwo.dojooverflow.services.DojoOverflowServices;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
-import javax.websocket.OnError;
-import java.util.List;
 
 @Controller
 public class dojoOverflowController {
